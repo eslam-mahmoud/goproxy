fc39e096ba24d9ff06f785a63759ae84e88b1cf9
Eslam Mahmoud <contact@eslam.me>
2021-10-10 20:11:14 -0500

    updates and readme

--- config.go
+++ config.go
@@ -1,35 +1,5 @@
 package main
 
-// import (
-// 	"flag"
-// 	"log"
-// 	"strings"
-// )
-
-// const defaultListen = ":8080"
-// const defaultProvider = "api.giphy.com:443"
-
-// var (
-// 	allowedProviders = []string{}
-// 	listen           = ""
-// )
-
-// func loadConfig() {
-// 	// return pointer
-// 	listen = *flag.String("listen", defaultListen, "The address and port to listen for http connections on")
-// 	providers := *flag.String("providers", defaultProvider, "comma separated string of enabled providers")
-
-// 	// parses from os.Args[1:]. Must be called after all flags are defined and before flags are accessed by the program.
-// 	flag.Parse()
-// 	allowedProviders = strings.Split(providers, ",")
-// 	log.Println("starting app")
-// 	log.Println("listen:", listen)
-// 	log.Println("providers:", allowedProviders)
-
-// 	// RUN
-// 	// go run main.go -listen=":8080" -providers="api.giphy.com:443,google.com:80"
-// }
-
 import (
 	"log"
 	"os"
@@ -37,8 +7,8 @@ import (
 )
 
 // set default values to be used if nothing was set from env vars
-const defaultListen = ":8080"
-const defaultProvider = "api.giphy.com:443"
+const defaultListen = ":8081"
+const defaultProvider = "api.giphy.com:443,google.com:443"
 
 var (
 	allowedProviders = []string{}
@@ -55,10 +25,9 @@ func loadConfig() {
 
 	providers := os.Getenv("PROVIDERS")
 	if providers == "" {
-		allowedProviders = append(allowedProviders, defaultProvider)
-	} else {
-		allowedProviders = strings.Split(providers, ",")
+		providers = defaultProvider
 	}
+	allowedProviders = strings.Split(providers, ",")
 
 	log.Println("starting app")
 	log.Println("listen:", listen)
--- main.go
+++ main.go
@@ -36,14 +36,6 @@ func handleReq(w http.ResponseWriter, req *http.Request) {
 
 // tunnel create a tunnel between the user and the
 func tunnel(w http.ResponseWriter, req *http.Request) {
-	// TODO check request done through postmans/insomnia VS CMD
-	// we get requestURI as "/api.giphy.com/v1/gifs/search?q=morning&api_key=123" from api client apps
-	// we get requestURI as "api.giphy.com:443" from CMD
-	// if string(req.RequestURI[0]) == "/" {
-	// log.Println(req.RequestURI) // for debuging
-	// 	req.RequestURI = req.RequestURI[1:]
-	// }
-
 	log.Println("Handleing req")
 
 	// Connect to destination
@@ -105,26 +97,3 @@ func main() {
 		panic(err)
 	}
 }
-
-// How to start the app
-/*
-https://linux.die.net/man/1/curl
--p/--proxytunnel
-    When an HTTP proxy is used (-x/--proxy), this option will cause non-HTTP protocols to attempt to tunnel through the proxy instead of merely using it to do HTTP-like operations.
-
-	The tunnel approach is made with the HTTP proxy CONNECT request and requires that the proxy allows direct connect to the remote port number curl wants to tunnel through to.
-
--x/--proxy <proxyhost[:port]>
-    Use the specified HTTP proxy. If the port number is not specified, it is assumed at port 1080.
-
-    This option overrides existing environment variables that set the proxy to use. If there's an environment variable setting a proxy, you can set proxy to "" to override it.
-
-    Note that all operations that are performed over a HTTP proxy will transparently be converted to HTTP. It means that certain protocol specific operations might not be available. This is not the case if you can tunnel through the proxy, as done with the -p/--proxytunnel option.
-
-    Starting with 7.14.1, the proxy host can be specified the exact same way as the proxy environment variables, including the protocol prefix (http://) and the embedded user + password.
-
-    If this option is used several times, the last one will be used.
-
-
-curl -p --proxy 127.0.0.1:12345 https://api.giphy.com/v1/gifs/search?q=morning\&api_key=123\&limit=1 | jq
-*/
